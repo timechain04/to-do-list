24eaa671a1947fbf18fb05434e334dc848d3e0ef
Andrii Lutsiak <timechain04@gmail.com>
2023-05-17 10:28:20 +0300

    main js

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ function checkEmptyList() {
         const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
         <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
         <div class="empty-list__title">To-do list is empty</div>
-    </li>`
+        </li>`
     tasksList.insertAdjacentHTML('afterbegin', emptyListHTML)
     }
 
